b6c64b94eb121117fd0c9766ce4331bf1866634d
TX_DROPDEAD <core.nexus.decode@gmail.com>
2020-10-02 13:20:56 -0300

    Se agregó Ciclo For en Python

---
+++ ejemplo-2.py
@@ -0,0 +1,15 @@
+
+ingresos=int(input("¿cuantos números desea ingresar?: "))
+
+par = 0
+inpar = 0
+
+for x in range(ingresos):
+    numero=int(input("Ingrese un número: "))
+    if (numero%2==0):
+        par=par+1
+    elif(numero%2!=0):
+        impar=impar+1
+
+print("La cantidad de números pares es: " + str(par))
+print("La cantidad de números impares es: " + str(impar))
